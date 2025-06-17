bbe5e866d412855719d9679d57e09083b1eb3f7e
erinshek <turdibekjumabaev05@gmail.com>
2025-06-18 00:58:22 +0500

    SHA256 key updated

--- homebrew-formula/cli-tool.rb
+++ homebrew-formula/cli-tool.rb
@@ -4,7 +4,7 @@ class CliTool < Formula
   desc "Latin-to-Cyrillic converter CLI for the Karakalpak language"
   homepage "https://github.com/dontbeidle/kaalin-homebrew"
   url "https://github.com/dontbeidle/kaalin-homebrew/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3590bcb90a75c32ba8b10d692d26838caedbc267a57db23931694abc9598c873"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on "python@3.11"
