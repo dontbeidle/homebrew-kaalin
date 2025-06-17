689ea5c083afae26dc3be51376e7122e1f3b30c2
erinshek <turdibekjumabaev05@gmail.com>
2025-06-18 01:16:19 +0500

    Add Kaalin formula for Latin-to-Cyrillic converter CLI

--- homebrew-formula/kaalin.rb
+++ homebrew-formula/kaalin.rb
@@ -1,26 +1,19 @@
-class CliTool < Formula
+class Kaalin < Formula
   include Language::Python::Virtualenv
-
+  
   desc "Latin-to-Cyrillic converter CLI for the Karakalpak language"
   homepage "https://github.com/dontbeidle/kaalin-homebrew"
   url "https://github.com/dontbeidle/kaalin-homebrew/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
-
-  depends_on "python@3.11"
-
-  resource "kaalin" do
-    url "https://github.com/dontbeidle/kaalin-python/archive/refs/tags/v3.1.0.tar.gz"
-    sha256 "e0b8b500eb1ec3d4406e83238b99c6f0a67c7d94192ea25aaf53f37236466b31"
-  end
-
+  
+  depends_on "python@3.9"
+  
   def install
     virtualenv_install_with_resources
   end
-
+  
   test do
-    (testpath/"test.txt").write("Сәлем әлем")
-    system bin/"cyr2lat", "test.txt", "out.txt"
-    assert_predicate testpath/"out.txt", :exist?
+    system bin/"kaalin", "--help"
   end
 end
